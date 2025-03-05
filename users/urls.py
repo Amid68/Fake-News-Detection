33b285ed2c463dc9c186eea52ace6f6e28297ecf
Amid Othman <othman.ameed@gmail.com>
2025-03-05 23:03:53 +0200

    update the users URLs

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('register/', views.register_view, name='register'),
+    path('profile/', views.profile_view, name='profile'),
 ]
