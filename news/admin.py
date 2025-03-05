68c96803c88fe8eb37ebbd202937bb2bdae3ac48
Amid Othman <othman.ameed@gmail.com>
2025-03-05 23:18:26 +0200

    update the admin interface

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Source, Article
 
-# Register your models here.
+
+@admin.register(Source)
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'base_url')
+    search_fields = ('name',)
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'source', 'publication_date', 'has_summary', 'bias_score')
+    list_filter = ('source', 'publication_date')
+    search_fields = ('title', 'content')
+    date_hierarchy = 'publication_date'
+
+    def has_summary(self, obj):
+        return bool(obj.summary)
+
+    has_summary.boolean = True
