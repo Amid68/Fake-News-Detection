98027ce0c6da4830b613f8d774f532a515196167
Amid Othman <othman.ameed@gmail.com>
2025-03-05 22:59:51 +0200

    update the admin.py to register CustomUser model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, UserPreference
 
-# Register your models here.
+class UserPreferenceInline(admin.TabularInline):
+    model = UserPreference
+    extra = 1
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'registration_date')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('registration_date',)}),
+    )
+    readonly_fields = ('registration_date',)
+    inlines = [UserPreferenceInline]
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(UserPreference)
