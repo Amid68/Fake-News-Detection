4ae5f89ec8c0b97ffaaeb517465ec6142d8e32e5
Amid Othman <othman.ameed@gmail.com>
2025-03-05 23:00:15 +0200

    update the views.py to improve the registration view

--- users/views.py
+++ users/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import CustomUserCreationForm
 
 def register_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')  # Define 'home' later
+            messages.success(request, f'Account created for {user.username}!')
+            return redirect('home')  # Redirect to home page
+        else:
+            # If form is not valid, errors will be displayed automatically
+            pass
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile_view(request):
+    return render(request, 'users/profile.html')
